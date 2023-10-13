233c48e8517d22260a894017904af0bae0a04a74
unknown <leejeff0319@gmail.com>
2023-10-13 16:51:20 -0400

    fixed config.js and config.ts files

--- next.config.js
+++ next.config.js
@@ -1,26 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+      config.module.rules.push({
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+      });
+      return config;
+  },
+};
 
-module.exports = nextConfig
-
-module.exports = {
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
-        })
-        return config
-    },
-}
-
-module.exports = {
-    theme: {
-      extend: {
-        fontSize: {
-          '2xs': '0.625rem', // This is 10px. Adjust if necessary.
-        },
-      },
-    },
-    variants: {},
-    plugins: [],
-  }
+module.exports = nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
       },
       fontSize: {
         'xxs': '0.60rem',  // Add this line
+        '2xs': '0.625rem', // Add this line
       },
     },
   },
